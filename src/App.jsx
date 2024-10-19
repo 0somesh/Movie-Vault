1dbedf9eb63ac76ee9dd273d63271c190ca8c5ca
0somesh <0somesh.sh@gmail.com>
2024-10-19 11:05:18 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Changes made in React file</h1>
+      <h1>Movie Vault!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
